9d586a27846a7eb3dc2076a981c75a07073e4a46
Momday <md@kaneoh.org>
2014-12-26 18:03:00 +0800

    Implemented a pseudo minimal case for the resizing function.

--- client/helpers/utilities.js
+++ client/helpers/utilities.js
@@ -2,7 +2,12 @@
 //A variable withouth the keyword var is global variable.
 // This global function resize the content of a grid-stack-content element
 // given its id (with the # sign eg: #qwrwsll12 )
+// TODO: Figure out the correct mininumHeight value.
+//
+//
 adjustGridStackContent = function (elementId) {
+
+        var mininumHeight = 200;
 
         // Tiny trick in case there is no elementId
         // then just add an empty space and this function
@@ -13,18 +18,21 @@ adjustGridStackContent = function (elementId) {
         }
 
         var totalHeight = $(prefix  +  ".grid-stack-item-content").height();
-        var headHeight  = $(prefix  +  ".grid-stack-item-content .header").height();
-        var tailHeight  = $(prefix  +  ".grid-stack-item-content .tail").height();
+        if (totalHeight <= mininumHeight) {
+            var headHeight  = 20;
+            var tailHeight  = 20;
+            totalHeight = mininumHeight;
+            var fillRemaining = totalHeight - headHeight - tailHeight - 15;
+        } else {
+            var headHeight  = $(prefix  +  ".grid-stack-item-content .header").height();
+            var tailHeight  = $(prefix  +  ".grid-stack-item-content .tail").height();
+            var fillRemaining = totalHeight - headHeight - tailHeight - 15;
+        }
 
-        console.log('totalHeight= ' + totalHeight);
+        /*console.log('totalHeight= ' + totalHeight);
         console.log('headHeight= ' + headHeight);
         console.log('tailHeight= ' + tailHeight);
+        console.log('fillRemaining: ' + fillRemaining);*/
 
-        var fillRemaining = totalHeight - headHeight - tailHeight - 15;
-        console.log('fillRemaining: ' + fillRemaining);
-
-
-
-        console.log(fillRemaining);
         $(prefix  +  ".grid-stack-item-content .content").css('height',fillRemaining);
 }
